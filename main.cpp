9c67686d10275d10936c09067200b73c4b4e4c39
nibchao <nibchao@gmail.com>
2022-03-02 22:14:20 -0800

    added functions to get input for creating a task, but need to add input validation for classification

--- main.cpp
+++ main.cpp
@@ -1,20 +1,57 @@
 #include <iostream>
+#include <sstream>
+#include "TaskList.hpp"
+#include "PersonalTask.hpp"
+#include "WorkTask.hpp"
+#include "StudyTask.hpp"
 
 using namespace std;
 
-int printMainMenu(); // function prototype
+// function prototypes
+int printMainMenu();
+string getTaskTitle();
+string getTaskDescription();
+string getTaskClassification();
+int getTaskPriority();
+int getTaskDuration();
+string getTaskDate();
+//
 
 int main()
 {
+	TaskList* tasklist = new TaskList();
+	string title = "";
+	string description = "";
+	string classification = "";
+	int priority = 0;
+	int duration = 0;
+	string date = "";
+
 	int input = 0;
 	do
 	{
 		input = printMainMenu();
 		switch (input)
 		{
-			case 1:
-				// do add task stuff
-				cout << "add a task here" << endl << endl;
+			case 1: // add a task by getting the task information
+				title = getTaskTitle();
+				description = getTaskDescription();
+				priority = getTaskPriority();
+				duration = getTaskDuration();
+				date = getTaskDate();
+				classification = getTaskClassification();
+				if (classification == "Personal")
+				{
+					TaskList->createPersonalTask(new PersonalTask(title, description, classification, priority, duration, date);
+				}
+				else if (classification == "Work")
+				{
+					TaskList->createWorkTask(new WorkTask(title, description, classification, priority, duration, date);
+				}
+				else if (classification == "Study")
+				{
+					TaskList->createStudyTask(new StudyTask(title, description, classification, priority, duration, date);
+				}
 				break;
 			case 2:
 				// print task stuff
@@ -56,4 +93,85 @@ int printMainMenu()
 	cin.ignore(10000, '\n');
 
 	return input;
+}
+
+string getTaskTitle()
+{
+	string taskTitle = "";
+	cout << "Enter the task title: ";
+	getline(cin, taskTitle);
+	cout << endl;
+	return taskTitle;
+}
+
+string getTaskDescription()
+{
+	string taskDescription = "";
+	cout << "Enter the task description: ";
+	getline(cin, taskDescription);
+	cout << endl;
+	return taskDescription;
+}
+
+string getTaskClassification()
+{
+	// add validation for only accepting personal, work, study as strings
+	string taskClassification = "";
+	cout << "Enter the task classification: ";
+	getline(cin, taskClassification);
+	cout << endl;
+	return taskClassification;
+}
+
+int getTaskPriority()
+{
+	int taskPriority = 0;
+	string str = "";
+	cout << "Enter the task priority: ";
+
+	while (getline(cin, str))
+	{
+		stringstream stream(str);
+		if (stream >> taskPriority)
+		{
+			if (stream.eof() && taskPriority > 0)
+			{
+				break;
+			}
+		}
+		cout << endl << "Error: Invalid task priority. Task priority must be a value greater than 0." << endl;
+		cout << endl << "Enter the task priority: ";
+	}
+	return taskPriority;
+}
+
+int getTaskDuration()
+{
+	int taskDuration = 0;
+	string str = "";
+	cout << "Enter the task duration (how many minutes it will take): ";
+
+	while (getline(cin, str))
+	{
+		stringstream stream(str);
+		if (stream >> taskDuration)
+		{
+			if (stream.eof() && taskDuration > 0)
+			{
+				break;
+			}
+		}
+		cout << endl << "Error: Invalid task duration. Task duration must be a value greater than 0 minutes." << endl;
+		cout << endl << "Enter the task duration (how many minutes it will take): ";
+	}
+	return taskDuration;
+}
+
+string getTaskDate()
+{
+	string taskDate = "";
+	cout << "Enther the task date: ";
+	getline(cin, taskDate);
+	cout << endl;
+	return taskDate;
 }
