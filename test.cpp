d7cd75081af85272194a49cd093dec856a180a3a
nibchao <nibchao@gmail.com>
2022-03-06 13:55:05 -0800

    added unit tests that should test functionality of tasklist when the needed files are implemented

--- test.cpp
+++ test.cpp
@@ -1,11 +1,56 @@
 #include "gtest/gtest.h"
 
+#include "TaskList.hpp"
+#include "WorkTask.h"
+#include "PrintTasks.h"
+#include "generalTask.hpp"
+#include "generalTask.cpp"
+#include "studytask.hpp"
 
-TEST(SuiteName, TestName)
+using namespace std
+
+TEST(TaskListTest, printBlankTaskList)
 {
-	EXPECT_EQ("1", "1");
+	TaskList TaskList;
+	TaskList.printTaskList();
 }
 
+TEST(TaskListTest, printPersonalTask)
+{
+	TaskList TaskList;
+	PersonalTask* task = new PersonalTask("p task title", "p task description", "Personal", 1, 20, "March 11");
+	TaskList.createPersonalTask(task);
+	TaskList.printTaskList();
+}
+
+TEST(TaskListTest, printWorkTask)
+{
+	TaskList TaskList;
+	WorkTask* task = new WorkTask("w task title", "w task description", "Work", 2, 30, "March 12");
+	TaskList.createWorkTask(task);
+	TaskList.printTaskList();
+}
+
+TEST(TaskListTest, printStudyTask)
+{
+	TaskList TaskList;
+	StudyTask* task = new StudyTask("s task title", "s task description", "study", 3, 40, "March 13");
+	TaskList.createStudyTask(task);
+	TaskList.printTaskList();
+}
+
+TEST(TaskListTest, printAllTask)
+{
+	TaskList TaskList;
+	PersonalTask* task = new PersonalTask("p task title", "p task description", "Personal", 1, 20, "March 11");
+	WorkTask* task = new WorkTask("w task title", "w task description", "Work", 2, 30, "March 12");
+	StudyTask* task = new StudyTask("s task title", "s task description", "study", 3, 40, "March 13");
+	TaskList.createPersonalTask(task);
+	TaskList.createWorkTask(task);
+	TaskList.createStudyTask(task);
+	TaskList.printTaskList();
+}
+	
 int main(int argc, char **argv)
 {
 	::testing::InitGoogleTest(&argc, argv);
