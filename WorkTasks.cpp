4adf697a3dbc08aa9278d154cfff284994f7fc53
EdgarNeyTapia <tapia.edgar.ney@gmail.com>
2022-03-07 17:12:28 -0800

    Work task cpp file

---
+++ WorkTasks.cpp
@@ -0,0 +1,21 @@
+#include "WorkTasks.h"
+#include <string>
+
+using namespace std;
+
+void WorkTask::howImportant(int importance){
+	urgency = importance;
+}
+
+void WorkTask::whatStatus(string status){
+	condition = status;
+}
+
+int WorkTask::getUrgency(){
+	return urgency;
+}
+
+string WorkTask::getCurrentCondition(){
+	return condition;
+}
+
