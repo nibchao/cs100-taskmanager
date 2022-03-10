61c93ce401f9b8959d1cc1a3e01da140eaacdddd
Nicholas Chao <57279548+nibchao@users.noreply.github.com>
2022-03-09 23:36:11 -0800

    Update test.cpp
    removed accidentally merged generalTask unit tests

--- test.cpp
+++ test.cpp
@@ -3,8 +3,6 @@
 #include "personalTask.cpp"
 #include "personalTask.hpp"
 
-#include "generalTask.hpp"
-#include "generalTask.cpp"
 #include <string>
 
 using namespace std;
@@ -22,35 +20,6 @@ TEST(PersonalTaskTest, Secondrun)
         task->setDate("January 1");
         EXPECT_EQ(task->getDate(), "January 1");
         delete task;
-}
-
-TEST(generalTaskTest, BlankInputs)
-{
-	generalTask* t = new generalTask("", "", "");
-	EXPECT_EQ(t->getTitle(), "");
-	EXPECT_EQ(t->getDescription(), "");
-	EXPECT_EQ(t->getClassification(), "");
-	delete t;
-}
-
-TEST(generalTaskTest, AllInputs)
-{
-	generalTask* t = new generalTask("Title", "Description", "personal");
-	EXPECT_EQ(t->getTitle(), "Title");
-	EXPECT_EQ(t->getDescription(), "Description");
-	EXPECT_EQ(t->getClassification(), "personal");
-	delete t;
-}
-
-TEST(generalTaskTest, editTaskInfo)
-{
-	generalTask* t = new generalTask("", "", "");
-	t->editTitle("new title");
-	t->editDescription("new desc");
-	EXPECT_EQ(t->getTitle(), "new title");
-  EXPECT_EQ(t->getDescription(), "new desc");
-  EXPECT_EQ(t->getClassification(), "");
-	delete t;
 }
 
 int main(int argc, char **argv)
