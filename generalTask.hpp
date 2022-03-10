0c2e7d55a0ccad683c4028e0c82a848f760fb297
nibchao <nibchao@gmail.com>
2022-03-09 23:40:31 -0800

    readded accidentally deleted generalTask files

---
+++ generalTask.hpp
@@ -0,0 +1,26 @@
+#ifndef __GENERALTASK_HPP__
+#define __GENERALTASK_HPP__
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class generalTask
+{
+	private:
+		string title;
+		string description;
+		string classification;
+	public:
+		generalTask(string, string, string);
+		~generalTask() { };
+		string getTitle();
+		string getDescription();
+		string getClassification();
+		void editTitle(string);
+		void editDescription(string);
+		void editClassification(string);
+};
+
+#endif
