db82f53b1691bf577177c7eedf4599abfa63dd94
nibchao <nibchao@gmail.com>
2022-03-09 22:54:10 -0800

    moved unit tests from separate file to test.cpp

--- test.cpp
+++ test.cpp
@@ -1,9 +1,18 @@
 #include "gtest/gtest.h"
+#include "WorkTasks.h"
+
+TEST(WorkTasks, testNum1){
+	WorkTasks* task = new WorkTasks("Zoom","Meeting with someone","Work", 8);
+	EXPECT_EQ(8,task->getPriority());
+	delete task;
+}
 
 
-TEST(SuiteName, TestName)
-{
-	EXPECT_EQ("1", "1");
+TEST(WorkTasks, testNum2){
+        WorkTasks* task = new WorkTasks("Zoom","Meeting with someone","work", 8);
+        task->setPriority(0);
+	EXPECT_EQ(0,task->getPriority());
+	delete task;
 }
 
 int main(int argc, char **argv)
