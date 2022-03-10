93f3428c8ac28cc1ccb01ac929ef9ff250a82e88
Etapi017 <99509609+Etapi017@users.noreply.github.com>
2022-03-09 17:59:39 -0800

    Update WorkTasks.hpp

--- WorkTasks.hpp
+++ WorkTasks.hpp
@@ -8,14 +8,19 @@ private:
 	int urgency;
 	string taskState;
 public:
-	WorkTask(string taskStr1, string taskStr2, string taskStr3, string taskStr4, int taskInt1, string taskStr5)
+	WorkTasks(string taskStr1, string taskStr2, string taskStr3, string taskStr4, int taskInt1, string taskStr5)
 		: generalTask(taskStr1,taskStr2,taskStr3,taskStr4), urgency(taskInt1), taskState(taskStr5) {}
-	~WorkTask(){
+	
+	~WorkTasks(){
 		taskState.clear();
 	}
+	
 	void howImportant(int);
+	
 	void whatState(string);
+	
 	int getHowImportant();
+	
 	string getCurrentStatus();
 };
 
